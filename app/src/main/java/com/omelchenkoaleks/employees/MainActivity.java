d0bf2cb5eef05bd565fa25c431d951da877e0417
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-08-12 09:38:28 +0300

    получаем данные и выводим их ...

--- app/src/main/java/com/omelchenkoaleks/employees/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/employees/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.omelchenkoaleks.employees.adapters.EmployeeAdapter;
+import com.omelchenkoaleks.employees.api.ApiFactory;
+import com.omelchenkoaleks.employees.api.ApiService;
 import com.omelchenkoaleks.employees.pojo.Employee;
+import com.omelchenkoaleks.employees.pojo.EmployeeResponse;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -23,9 +30,43 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = findViewById(R.id.employees_recycler_view);
         mEmployeeAdapter = new EmployeeAdapter();
+        mEmployeeAdapter.setEmployees(new ArrayList<Employee>());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mEmployeeAdapter);
 
+        /*
+            после этих двух строчек мы можем получать наши данные,
+            используя реализованные методы ApiService Ретрофитом ...
+          */
+        ApiFactory apiFactory = ApiFactory.getInstance();
+        ApiService apiService = apiFactory.getApiService();
+
+        // ВОТ ТАК:
+
+        /*
+            первые две строчки стандартные:
+                1 = в каком потоке загружать данные
+                2 = в каком потоке выводить данные
+
+                теперь указать, что делать когда мы получим данные:
+                    используем метод subscribe с двумя параметрами, передаем два объекта
+                    анонимного класса Consumer (первый выполняется в случае, если данные загружены
+                    успешно, второй принимает исключение)
+         */
+        apiService.getEmployees()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<EmployeeResponse>() {
+                    @Override
+                    public void accept(EmployeeResponse employeeResponse) throws Exception {
+                        mEmployeeAdapter.setEmployees(employeeResponse.getResponse());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Toast.makeText(MainActivity.this, "Ошибка получения данных!", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 
         /* ------- Тест, чтобы проверить что работает: ------- */
